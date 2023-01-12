af199bd038953bb9528aa9bddbb40482b84181bc
Adam Ulrich <adamulrich@hotmail.com>
2023-01-11 21:22:40 -0800

    adding window. to localstorage.

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -7,17 +7,18 @@ export function qs(selector, parent = document) {
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
-  return JSON.parse(localStorage.getItem(key));
+  return JSON.parse(window.localStorage.getItem(key));
 }
 // save data to local storage
 export function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
+  window.localStorage.setItem(key, JSON.stringify(data));
 }
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
-  qs(selector).addEventListener("touchend", (event) => {
+  qs(selector).addEventListener('touchend', (event) => {
     event.preventDefault();
     callback();
   });
-  qs(selector).addEventListener("click", callback);
+  qs(selector).addEventListener('click', callback);
 }
+  
