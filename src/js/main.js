d6f218f34c03dbad6592df208ee08073321fa44a
Adam Ulrich <adamulrich@hotmail.com>
2023-01-12 22:19:29 -0800

    fixing reload by moving to cart update

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,10 @@
 // Superscript number on cart
-let ssn = document.getElementById("superscriptnum");
+let ssn = document.getElementById('superscriptnum');
 function displaySuperscriptNumber() {
-    let items = JSON.parse(localStorage.getItem("so-cart"));
+    let items = JSON.parse(localStorage.getItem('so-cart'));
     if (items != null) {
-        let number = items.length
-        ssn.innerHTML = number
-        // location.reload()
+        let number = items.length;
+        ssn.innerHTML = number;
     } else {
         return
     }
