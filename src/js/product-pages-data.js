0a18cf5ebc6b01fbfaa269bc10fba2e36f6edce0
Adam Ulrich <adamulrich@hotmail.com>
2023-01-18 18:32:12 -0800

    week3 assigment

--- src/js/product-pages-data.js
+++ src/js/product-pages-data.js
@@ -2,9 +2,11 @@ import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
-async function buildDetailsPage(e) {
-    const product = await dataSource.findProductById(e.target.dataset.id);
-    // document.getElementById('addToCart').data-id = e.target.dataset.id;
+async function buildDetailsPage() {
+
+    const productId = new URLSearchParams(document.location.search).get('productId');
+    const product = await dataSource.findProductById(productId);
+    document.getElementById('addToCart').setAttribute('data-id',productId);
     document.getElementById('product-descriptionhtmlsimple').innerHTML = product.DescriptionHtmlSimple;
     document.getElementById('product-color-name').innerText = product.Colors.ColorName;
     document.getElementById('product-listprice').innerText = product.ListPrice;
@@ -12,6 +14,6 @@ async function buildDetailsPage(e) {
     document.getElementById('product-image').alt = product.Name;
     document.getElementById('product-name-without-brand').innerText = product.NameWithoutBrand;
     document.getElementById('product-brand-name').innerText = product.Brand.Name;
-    document.getElementById('product-title').innerText = product.Name;
+    document.title = 'Sleep Outside | ' + product.Name;
 }
 buildDetailsPage()
