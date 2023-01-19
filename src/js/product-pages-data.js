1e21884fa0c3869368e45ac8e0171c1e0762285e
AbelAguilarAguirre <ulysses.aguilar.aguirre@gmail.com>
2023-01-18 15:49:21 -1000

    Product page almost set up html

--- src/js/product-pages-data.js
+++ src/js/product-pages-data.js
@@ -4,5 +4,14 @@ const dataSource = new ProductData('tents');
 
 async function buildDetailsPage(e) {
     const product = await dataSource.findProductById(e.target.dataset.id);
-    addProductToCart(product);
+    // document.getElementById('addToCart').data-id = e.target.dataset.id;
+    document.getElementById('product-descriptionhtmlsimple').innerHTML = product.DescriptionHtmlSimple;
+    document.getElementById('product-color-name').innerText = product.Colors.ColorName;
+    document.getElementById('product-listprice').innerText = product.ListPrice;
+    document.getElementById('product-image').src = product.Image;
+    document.getElementById('product-image').alt = product.Name;
+    document.getElementById('product-name-without-brand').innerText = product.NameWithoutBrand;
+    document.getElementById('product-brand-name').innerText = product.Brand.Name;
+    document.getElementById('product-title').innerText = product.Name;
 }
+buildDetailsPage()
