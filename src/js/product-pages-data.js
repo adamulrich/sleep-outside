2e50796c9e8e5bb5d0f1dad1578aa0d8dfd72d10
AbelAguilarAguirre <ulysses.aguilar.aguirre@gmail.com>
2023-01-19 23:34:36 -1000

    Added html to add the discounted price
    added css to style the price
    
    added js to add the discounted price.

--- src/js/product-pages-data.js
+++ src/js/product-pages-data.js
@@ -9,11 +9,12 @@ async function buildDetailsPage() {
     document.getElementById('addToCart').setAttribute('data-id',productId);
     document.getElementById('product-descriptionhtmlsimple').innerHTML = product.DescriptionHtmlSimple;
     document.getElementById('product-color-name').innerText = product.Colors[0].ColorName;
-    document.getElementById('product-listprice').innerText = product.ListPrice;
+    document.getElementById('product-suggested-retail-price').innerText = `$${product.SuggestedRetailPrice}`;
     document.getElementById('product-image').src = product.Image;
     document.getElementById('product-image').alt = product.Name;
     document.getElementById('product-name-without-brand').innerText = product.NameWithoutBrand;
     document.getElementById('product-brand-name').innerText = product.Brand.Name;
     document.title = 'Sleep Outside | ' + product.Name;
+    document.getElementById('product-final-price').innerText = `$${product.FinalPrice}`;
 }
 buildDetailsPage()
