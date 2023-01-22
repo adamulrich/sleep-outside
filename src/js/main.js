f0b17db77128bbae01408dd81b1d5fc3a87e6886
Chais Chang <55469731+isthatchais@users.noreply.github.com>
2023-01-21 20:20:38 -0700

    added Quantity to the objects in cart (#14)
    updated addProductToCart to use Quantity
    updated Main.js to use Quantity
    updated cart.js to use Quantity

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,18 @@
 // Superscript number on cart
+import { getLocalStorage } from './utils.mjs';
+
 const ssn = document.getElementById('superscriptnum');
 if (ssn.textContent === '') {
   ssn.setAttribute('class', 'is-empty');
 }
 export function displaySuperscriptNumber() {
-  let items = JSON.parse(localStorage.getItem('so-cart'));
+  let items = getLocalStorage('so-cart');
   if (items != null) {
     ssn.classList.remove('is-empty');
-    let number = items.length;
+    let number = 0;
+    for(let i = 0; i < items.length; i++){
+      number += parseFloat(items[i].Quantity);
+    }
     ssn.innerHTML = number;
   } else {
     return;
