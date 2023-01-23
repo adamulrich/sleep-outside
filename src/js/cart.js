2b56fa396a901a4e3dd7d0b20e97d67a8b930f19
adamulrich <adamulrich@hotmail.com>
2023-01-22 19:54:49 -0800

    delete from cart card

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,4 @@
-import { getLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 
 function renderCartContents() {
   const cartItems = getLocalStorage('so-cart');
@@ -6,6 +6,18 @@ function renderCartContents() {
   if (cartItems != null) {
     htmlItems = cartItems.map((item) => cartItemTemplate(item));
     document.querySelector('.product-list').innerHTML = htmlItems.join('');
+
+    const deleteButtons = document.querySelectorAll('.cart-card__delete');
+    const deleteClickHandler = (e) => {
+      deleteCartItem(e);
+    }
+    let i = 0;
+    for (i = 0; i < deleteButtons.length; i++) {
+      deleteButtons[i].addEventListener('click', deleteClickHandler);
+      
+    }
+
+
   }
 }
 
@@ -23,6 +35,7 @@ function cartItemTemplate(item) {
   <p class='cart-card__color'>${item.Colors[0].ColorName}</p>
   <p class='cart-card__quantity'>qty: ${item.Quantity}</p>
   <p class='cart-card__price'>$${item.FinalPrice}</p>
+  <button data-id='${item.Id}' class='cart-card__delete'>X</button>
 </li>`;
 
   return newItem;
@@ -43,6 +56,15 @@ function displayTotal() {
     total += parseFloat(cartItems[i].FinalPrice);
   }
   totalSum.innerText = Math.round(total * 100) / 100;
+}
+
+function deleteCartItem(e) {
+  const cartItems = getLocalStorage('so-cart');
+  var index = cartItems.findIndex(obj => obj.name == e['data-id']) ;
+  cartItems.splice(index,1);
+  setLocalStorage('so-cart', cartItems);
+  renderCartContents();
+  displayTotal();
 }
 
 displayTotal();
