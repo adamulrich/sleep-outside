e3ac359b5338793e414eff2cabdc1aa1fb41324e
Adam Ulrich <adamulrich@hotmail.com>
2023-01-25 19:25:42 -0800

    changing indentation from 2 to 4

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,27 +1,27 @@
 function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error('Bad Response');
-  }
+    if (res.ok) {
+        return res.json();
+    } else {
+        throw new Error('Bad Response');
+    }
 }
 
 export default class ProductData {
-  constructor(category) {
-    this.category = category;
-    this.path = `../json/${this.category}.json`;
-  }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
-  }
-  async findProductById(id) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === id);
-  }
-  async findAllProducts() {
-    const products = await this.getData();
-    return products;
-  }
+    constructor(category) {
+        this.category = category;
+        this.path = `../json/${this.category}.json`;
+    }
+    getData() {
+        return fetch(this.path)
+            .then(convertToJson)
+            .then((data) => data);
+    }
+    async findProductById(id) {
+        const products = await this.getData();
+        return products.find((item) => item.Id === id);
+    }
+    async findAllProducts() {
+        const products = await this.getData();
+        return products;
+    }
 }
